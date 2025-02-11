bdadcb3754d9eda8dbd0563e3550444d34ba9913
GSB0203 <gimseungbin0203@gmail.com>
2025-02-11 21:43:36 +0900

    feat(Apron): ApronInResponse
    출발과 도착 항목이 달라 나누어서 구현하였습니다.

---
+++ src/main/java/com/airportpus/domain/apron/presentation/dto/ApronInResponse.java
@@ -0,0 +1,41 @@
+package com.airportpus.domain.apron.presentation.dto;
+
+import com.airportpus.domain.apron.service.dto.ApronApiResponse;
+
+public record ApronInResponse(
+    String flightNumber,
+    String airlineEnglish,
+    String airlineKorean,
+    String arrivedEng,
+    String arrivedKor,
+    String baggageClaim,
+    String boardingEng,
+    String boardingKor,
+    String std,
+    String etd,
+    String io,
+    String line,
+    String remarkEng,
+    String remarkKor
+
+) {
+
+  public static ApronInResponse from(ApronApiResponse.FlightInfo info) {
+    return new ApronInResponse(
+        info.getFlightNumber(),
+        info.getAirlineEnglish(),
+        info.getAirlineKorean(),
+        info.getArrivedEng(),
+        info.getArrivedKor(),
+        info.getBaggageClaim(),
+        info.getBoardingEng(),
+        info.getBoardingKor(),
+        info.getStd(),
+        info.getEtd(),
+        info.getIo(),
+        info.getLine(),
+        info.getRemarkEng(),
+        info.getRemarkKor()
+    );
+  }
+}
