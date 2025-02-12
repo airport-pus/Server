23a8a556a27a647a3d2c8cc988edf237f932e953
GSB0203 <gimseungbin0203@gmail.com>
2025-02-13 01:30:22 +0900

    feat(swagger): ParkingResponse
    swagger를 적용하였습니다.

--- src/main/java/com/airportpus/domain/parking/presentation/dto/ParkingResponse.java
+++ src/main/java/com/airportpus/domain/parking/presentation/dto/ParkingResponse.java
@@ -2,14 +2,29 @@ package com.airportpus.domain.parking.presentation.dto;
 
 import com.airportpus.domain.parking.domain.Parking;
 import com.airportpus.domain.parking.service.dto.ParkingApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "주차장 정보 응답 DTO")
 public record ParkingResponse(
+
+    @Schema(description = "주차장명", example = "P1 여객주차장")
     String parkingAirportCodeName,
+
+    @Schema(description = "주차 혼잡도 상태 (원활, 혼잡, 만차)", example = "혼잡")
     String parkingCongestion,
+
+    @Schema(description = "주차 혼잡도 지수 (0~100)", example = "60")
     String parkingCongestionDegree,
+
+    @Schema(description = "현재 주차된 차량 수", example = "450")
     int parkingOccupiedSpace,
+
+    @Schema(description = "주차장 총 주차 가능 대수", example = "500")
     int parkingTotalSpace,
+
+    @Schema(description = "현재 남은 주차 공간 수", example = "50")
     int remainingSpace
+
 ) {
 
   public static ParkingResponse fromByParkingInfo(ParkingApiResponse.ParkingInfo parkingInfo) {
