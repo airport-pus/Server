fa7c53b0e4c6a554a712e4b99f49c02de56dba73
GSB0203 <gimseungbin0203@gmail.com>
2025-02-13 01:27:12 +0900

    feat(swagger): CongestionResponse
    swagger를 적용하였습니다.

--- src/main/java/com/airportpus/domain/congestion/presentation/dto/CongestionResponse.java
+++ src/main/java/com/airportpus/domain/congestion/presentation/dto/CongestionResponse.java
@@ -1,12 +1,14 @@
 package com.airportpus.domain.congestion.presentation.dto;
 
 import com.airportpus.domain.congestion.service.dto.CongestionApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "공항 혼잡도 응답 DTO")
 public record CongestionResponse(
-    int cgdrAllLvl,
-    int cgdrALvl,
-    int cgdrBLvl,
-    int cgdrCLvl
+    @Schema(description = "전체구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "4") int cgdrAllLvl,
+    @Schema(description = "1구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "1") int cgdrALvl,
+    @Schema(description = "2구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "2") int cgdrBLvl,
+    @Schema(description = "3구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "3") int cgdrCLvl
 ) {
 
   public static CongestionResponse from(CongestionApiResponse.CongestionInfo info) {
