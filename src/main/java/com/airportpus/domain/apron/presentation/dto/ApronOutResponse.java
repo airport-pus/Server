11ec538b7fb8029671483a870a9ec7bb422e40a2
GSB0203 <gimseungbin0203@gmail.com>
2025-02-13 01:26:53 +0900

    feat(swagger): ApronOutResponse
    swagger를 적용하였습니다.

--- src/main/java/com/airportpus/domain/apron/presentation/dto/ApronOutResponse.java
+++ src/main/java/com/airportpus/domain/apron/presentation/dto/ApronOutResponse.java
@@ -1,22 +1,24 @@
 package com.airportpus.domain.apron.presentation.dto;
 
 import com.airportpus.domain.apron.service.dto.ApronApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "주기장 도착 응답 DTO")
 public record ApronOutResponse(
-    String flightNumber,
-    String airlineEnglish,
-    String airlineKorean,
-    String arrivedEng,
-    String arrivedKor,
-    String gate,
-    String boardingEng,
-    String boardingKor,
-    String std,
-    String etd,
-    String io,
-    String line,
-    String remarkEng,
-    String remarkKor
+    @Schema(description = "항공편 번호", example = "KE123") String flightNumber,
+    @Schema(description = "항공사 영문명", example = "Korean Air") String airlineEnglish,
+    @Schema(description = "항공사 한글명", example = "대한항공") String airlineKorean,
+    @Schema(description = "도착 영문 정보", example = "Incheon") String arrivedEng,
+    @Schema(description = "도착 한글 정보", example = "인천") String arrivedKor,
+    @Schema(description = "탑승 게이트", example = "A12") String gate,
+    @Schema(description = "탑승 영문 정보", example = "Gimhae") String boardingEng,
+    @Schema(description = "탑승 한글 정보", example = "부산/김해") String boardingKor,
+    @Schema(description = "예정 출발 시간", example = "1800") String std,
+    @Schema(description = "예상 출발 시간", example = "1810") String etd,
+    @Schema(description = "입/출국 구분", example = "O") String io,
+    @Schema(description = "노선 정보", example = "국제") String line,
+    @Schema(description = "영문 비고", example = "DEPARTED") String remarkEng,
+    @Schema(description = "한글 비고", example = "출발") String remarkKor
 ) {
 
   public static ApronOutResponse from(ApronApiResponse.FlightInfo info) {
