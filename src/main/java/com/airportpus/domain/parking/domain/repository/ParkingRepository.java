9761c6e62bc52c2a1dd056d912900d7451af120a
GSB0203 <gimseungbin0203@gmail.com>
2025-02-13 01:31:10 +0900

    feat(Parking): ParkingRepository
    @Repository 어노테이션을 추가하였습니다.

--- src/main/java/com/airportpus/domain/parking/domain/repository/ParkingRepository.java
+++ src/main/java/com/airportpus/domain/parking/domain/repository/ParkingRepository.java
@@ -2,6 +2,8 @@ package com.airportpus.domain.parking.domain.repository;
 
 import com.airportpus.domain.parking.domain.Parking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParkingRepository extends JpaRepository<Parking, Long> {
 }
