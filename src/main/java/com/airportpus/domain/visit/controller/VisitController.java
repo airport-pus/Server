0fec1e56c6130389df9cd3c1ebf9cb2fe58469d3
GSB0203 <gimseungbin0203@gmail.com>
2025-02-13 01:30:33 +0900

    feat(swagger): VisitController
    swagger를 적용하였습니다.

--- src/main/java/com/airportpus/domain/visit/controller/VisitController.java
+++ src/main/java/com/airportpus/domain/visit/controller/VisitController.java
@@ -2,15 +2,15 @@ package com.airportpus.domain.visit.controller;
 
 import com.airportpus.domain.visit.service.CookieService;
 import com.airportpus.domain.visit.service.VisitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Visit API", description = "방문자 API")
 @RestController
 @RequestMapping("/visit")
 @RequiredArgsConstructor
@@ -19,13 +19,18 @@ public class VisitController {
   private final VisitService visitorService;
   private final CookieService cookieService;
 
+  @Operation(summary = "총 방문자 수 조회", description = "총 방문자 수를 조회합니다.")
   @GetMapping(produces = "application/json")
   public long getVisit() {
     return visitorService.getTotalVisitCount();
   }
 
+  @Operation(summary = "방문 여부 체크", description = "첫 방문이면 방문자를 증가시킵니다.")
   @PostMapping(produces = "application/json")
-  public ResponseEntity<String> checkVisit(HttpServletRequest request, HttpServletResponse response) {
+  public ResponseEntity<String> checkVisit(
+      @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "방문 체크 요청", required = true)
+      HttpServletRequest request, HttpServletResponse response) {
+
     if (!cookieService.hasVisitCookie(request)) {
       visitorService.incrementVisitCount();
       cookieService.createVisitCookie(response);
