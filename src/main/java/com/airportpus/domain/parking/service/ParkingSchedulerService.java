84a2febe7948bdcd34e9bf01436aa08299efe604
GSB0203 <gimseungbin0203@gmail.com>
2025-02-12 20:47:50 +0900

    feat(Parking): ParkingSchedulerService
    주차장 혼잡도를 저장하기 위해 구현하였습니다.

---
+++ src/main/java/com/airportpus/domain/parking/service/ParkingSchedulerService.java
@@ -0,0 +1,36 @@
+package com.airportpus.domain.parking.service;
+
+import com.airportpus.domain.parking.domain.Parking;
+import com.airportpus.domain.parking.domain.repository.ParkingRepository;
+import com.airportpus.domain.parking.presentation.dto.ParkingResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ParkingSchedulerService {
+
+  private final ParkingService parkingService;
+  private final ParkingRepository parkingRepository;
+
+  @Scheduled(cron = "0 0 * * * *")
+  public void saveParking() {
+    List<ParkingResponse> parkingInfoList = parkingService.getParkingInfo();
+    parkingInfoList.forEach(parkingResponse -> {
+      Parking parking = Parking.builder()
+          .airportCodeName(parkingResponse.parkingAirportCodeName())
+          .congestion(parkingResponse.parkingCongestion())
+          .congestionDegree(parkingResponse.parkingCongestionDegree())
+          .totalSpace(parkingResponse.parkingTotalSpace())
+          .remainingSpace(parkingResponse.remainingSpace())
+          .occupiedSpace(parkingResponse.parkingOccupiedSpace())
+          .build();
+      parkingRepository.save(parking);
+    });
+  }
+}
