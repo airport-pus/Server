f49c924e0b06d8fc50d1b775af0604e57ff9a261
GSB0203 <gimseungbin0203@gmail.com>
2025-02-12 20:48:19 +0900

    refactor(Parking): ApiProperties
    yml 경로의 수정이 있었습니다.

--- src/main/java/com/airportpus/common/config/ApiProperties.java
+++ src/main/java/com/airportpus/common/config/ApiProperties.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 @Setter
 @Configuration
-@ConfigurationProperties(prefix = "api")
+@ConfigurationProperties(prefix = "spring.api")
 public class ApiProperties {
 
   private String serviceKey;
