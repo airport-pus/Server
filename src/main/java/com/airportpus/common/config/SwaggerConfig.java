9431701676cedf674d4821bb146b354c311067a4
GSB0203 <gimseungbin0203@gmail.com>
2025-02-14 03:14:55 +0900

    feat(Build): SwaggerConfig
    배포를 위해 추가하였습니다.

--- src/main/java/com/airportpus/common/config/SwaggerConfig.java
+++ src/main/java/com/airportpus/common/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.airportpus.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -30,5 +33,10 @@ import org.springframework.context.annotation.Configuration;
     )
 )
 public class SwaggerConfig {
-}
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .addServersItem(new Server().url("/").description("API Server"));
+    }
+}
