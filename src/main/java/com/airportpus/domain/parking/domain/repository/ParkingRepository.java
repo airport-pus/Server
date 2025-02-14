426169cf506dea6e06e86af9e2f3b3a8865136e9
GSB0203 <gimseungbin0203@gmail.com>
2025-02-14 12:36:50 +0900

    remove(Parking): ParkingRepository
    잘못된 기능 구현이어서 제거하였습니다.

--- src/main/java/com/airportpus/domain/parking/domain/repository/ParkingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airportpus.domain.parking.domain.repository;
-
-import com.airportpus.domain.parking.domain.Parking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingRepository extends JpaRepository<Parking, Long> {
-}
