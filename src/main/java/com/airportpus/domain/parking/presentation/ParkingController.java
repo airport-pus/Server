74975cf751d0a7d3c6f8c078e5ebe282a75f2509
GSB0203 <gimseungbin0203@gmail.com>
2025-02-14 12:37:47 +0900

    remove(Parking): ParkingController
    잘못된 기능 구현이어서 제거하였습니다.

--- src/main/java/com/airportpus/domain/parking/presentation/ParkingController.java
+++ src/main/java/com/airportpus/domain/parking/presentation/ParkingController.java
@@ -13,27 +13,21 @@ import java.util.List;
 
 @Tag(name = "Parking API", description = "주차장 API")
 @RestController
-@RequestMapping("/parkings")
+@RequestMapping("/parking")
 @RequiredArgsConstructor
 public class ParkingController {
 
   private final ParkingService parkingService;
   private final ParkingFeeService parkingFeeService;
 
-  @Operation(summary = "저장된 주차장 혼잡도 조회", description = "주차장의 저장된 혼잡도 정보를 조회합니다.")
+  @Operation(summary = "공항 주차장 정보 조회", description = "실시간 주차장 정보를 조회합니다.")
   @GetMapping(produces = "application/json")
-  public List<ParkingResponse> getAllParkingByCongestion() {
-    return parkingService.findAll();
-  }
-
-  @Operation(summary = "실시간 공항 주차장 정보 조회", description = "실시간 주차장 정보를 조회합니다.")
-  @GetMapping(value = "/real", produces = "application/json")
-  public List<ParkingResponse> getParkingByRealTime() {
-    return parkingService.getParkingByRealTime();
+  public List<ParkingResponse> getParking() {
+    return parkingService.getParking();
   }
 
   @Operation(summary = "주차 요금 계산", description = "입력된 정보를 바탕으로 주차 요금을 계산합니다.")
-  @PostMapping(value = "/fee", produces = "application/json")
+  @PostMapping(produces = "application/json")
   public int getParkingFee(
       @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "주차 요금 요청 데이터", required = true)
       @RequestBody ParkingFeeRequest request) {
