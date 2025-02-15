b33c0be8783c495c111579a1e15c5c05eafa34f6
GSB0203 <gimseungbin0203@gmail.com>
2025-02-15 20:59:00 +0900

    feat(Holiday): HolidayResponse
    스웨거를 추가하였습니다.

--- src/main/java/com/airportpus/domain/holiday/presentation/dto/HolidayResponse.java
+++ src/main/java/com/airportpus/domain/holiday/presentation/dto/HolidayResponse.java
@@ -1,9 +1,10 @@
 package com.airportpus.domain.holiday.presentation.dto;
 
 import com.airportpus.domain.holiday.service.dto.HolidayApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record HolidayResponse(
-    String holiday
+    @Schema(description = "휴일날짜", example = "20250101") String holiday
 ) {
 
   public static HolidayResponse from(HolidayApiResponse.HolidayInfo info) {
