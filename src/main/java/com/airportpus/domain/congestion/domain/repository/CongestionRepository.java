40f2ea6af0f00bbdb0bd908247effacb7e6834ca
GSB0203 <gimseungbin0203@gmail.com>
2025-02-15 19:14:01 +0900

    feat(Congestion): CongestionRepository
    어제 자정 전 생성된 데이터 (2일 전 데이터)를 제거하기 위해 추가하였습니다.

--- src/main/java/com/airportpus/domain/congestion/domain/repository/CongestionRepository.java
+++ src/main/java/com/airportpus/domain/congestion/domain/repository/CongestionRepository.java
@@ -4,6 +4,9 @@ import com.airportpus.domain.congestion.domain.Congestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface CongestionRepository extends JpaRepository<Congestion, Long> {
+  void deleteByCreatedAtBefore(LocalDateTime cutoff);
 }
