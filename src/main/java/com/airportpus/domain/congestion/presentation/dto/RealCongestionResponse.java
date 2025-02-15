a604f11128df645f6cfe754a0f06118463cbd205
GSB0203 <gimseungbin0203@gmail.com>
2025-02-15 18:40:46 +0900

    refactor(Congestion): RealCongestionResponse
    저장된 공항 혼잡도와 실기간 공항 혼잡도의 값이 다르기 때문에 분리하였습니다.

--- src/main/java/com/airportpus/domain/congestion/presentation/dto/RealCongestionResponse.java
+++ src/main/java/com/airportpus/domain/congestion/presentation/dto/RealCongestionResponse.java
@@ -1,32 +1,22 @@
 package com.airportpus.domain.congestion.presentation.dto;
 
-import com.airportpus.domain.congestion.domain.Congestion;
 import com.airportpus.domain.congestion.service.dto.CongestionApiResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "공항 혼잡도 응답 DTO")
-public record CongestionResponse(
+@Schema(description = "실시간 공항 혼잡도 응답 DTO")
+public record RealCongestionResponse(
     @Schema(description = "전체구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "4") int cgdrAllLvl,
     @Schema(description = "1구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "1") int cgdrALvl,
     @Schema(description = "2구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "2") int cgdrBLvl,
     @Schema(description = "3구간 혼잡도 레벨(1:원활 , 2:보통, 3:혼잡, 4:매우혼잡)", example = "3") int cgdrCLvl
 ) {
 
-  public static CongestionResponse fromByInfo(CongestionApiResponse.CongestionInfo info) {
-    return new CongestionResponse(
+  public static RealCongestionResponse from(CongestionApiResponse.CongestionInfo info) {
+    return new RealCongestionResponse(
         info.cgdrAllLvl(),
         info.cgdrALvl(),
         info.cgdrBLvl(),
         info.cgdrCLvl()
-    );
-  }
-
-  public static CongestionResponse fromByCongestion(Congestion congestion) {
-    return new CongestionResponse(
-        congestion.getCgdrAllLvl(),
-        congestion.getCgdrALvl(),
-        congestion.getCgdrBLvl(),
-        congestion.getCgdrCLvl()
     );
   }
 }
