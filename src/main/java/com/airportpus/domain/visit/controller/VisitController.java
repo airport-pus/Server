5f795122e62d269b1f3db0c16df79a2037a9df41
GSB0203 <gimseungbin0203@gmail.com>
2025-02-22 14:50:02 +0900

    feat(Visit): VisitController
    방문자 인증에 필요한 쿠키가 발급되지 않는 문제를 해결하기 위해 추가하였습니다.

--- src/main/java/com/airportpus/domain/visit/controller/VisitController.java
+++ src/main/java/com/airportpus/domain/visit/controller/VisitController.java
@@ -7,10 +7,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Visit API", description = "방문자 API")
+@Slf4j
 @RestController
 @RequestMapping("/visit")
 @RequiredArgsConstructor
@@ -22,21 +24,21 @@ public class VisitController {
   @Operation(summary = "총 방문자 수 조회", description = "총 방문자 수를 조회합니다.")
   @GetMapping(produces = "application/json")
   public long getVisit() {
-    return visitorService.getTotalVisitCount();
+    long totalVisit = visitorService.getTotalVisitCount();
+    log.info("총 방문자 수 조회: {}", totalVisit);
+    return totalVisit;
   }
 
   @Operation(summary = "방문 여부 체크", description = "첫 방문이면 방문자를 증가시킵니다.")
   @PostMapping(produces = "application/json")
-  public ResponseEntity<String> checkVisit(
-      @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "방문 체크 요청", required = true)
-      HttpServletRequest request, HttpServletResponse response) {
-
+  public ResponseEntity<String> checkVisit(HttpServletRequest request, HttpServletResponse response) {
     if (!cookieService.hasVisitCookie(request)) {
+      log.info("첫 방문으로 판단하여 방문자 수 증가 및 쿠키 생성 처리 시작.");
       visitorService.incrementVisitCount();
       cookieService.createVisitCookie(response);
       return ResponseEntity.ok("첫 방문");
     }
-
+    log.info("이미 방문 기록이 존재합니다.");
     return ResponseEntity.ok("방문기록 존재");
   }
 }
