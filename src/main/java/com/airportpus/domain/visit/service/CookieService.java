24ccb957324c4f94a16306e9492694b7a70cb982
GSB0203 <gimseungbin0203@gmail.com>
2025-02-22 15:45:20 +0900

    Revert "fix(Visit): CookieService"
    This reverts commit 2a42e62f62043f28e1e3cac80f0a3887769df68e.

--- src/main/java/com/airportpus/domain/visit/service/CookieService.java
+++ src/main/java/com/airportpus/domain/visit/service/CookieService.java
@@ -1,5 +1,6 @@
 package com.airportpus.domain.visit.service;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,10 @@ public class CookieService {
 
   private static final String VISITOR_COOKIE = "visited";
   private static final int COOKIE_EXPIRATION = 60 * 60;
-  private static final String DOMAIN = "airport-pus.kr";
 
   public boolean hasVisitCookie(HttpServletRequest request) {
     if (request.getCookies() != null) {
-      for (jakarta.servlet.http.Cookie cookie : request.getCookies()) {
+      for (Cookie cookie : request.getCookies()) {
         if (VISITOR_COOKIE.equals(cookie.getName())) {
           log.info("방문 쿠키 발견: {}", cookie.getName());
           return true;
@@ -28,15 +28,13 @@ public class CookieService {
   }
 
   public void createVisitCookie(HttpServletResponse response) {
-    ResponseCookie cookie = ResponseCookie.from(VISITOR_COOKIE, "true")
-        .domain(DOMAIN)
-        .maxAge(COOKIE_EXPIRATION)
-        .path("/")
-        .httpOnly(true)
-        .sameSite("None")
-        .build();
-
-    response.addHeader("Set-Cookie", cookie.toString());
-    log.info("방문 쿠키 생성: {}", cookie.getName());
+    Cookie cookie = new Cookie(VISITOR_COOKIE, "true");
+    cookie.setMaxAge(COOKIE_EXPIRATION);
+    cookie.setPath("/");
+    cookie.setHttpOnly(true);
+    response.addCookie(cookie);
   }
 }
+
+
+
