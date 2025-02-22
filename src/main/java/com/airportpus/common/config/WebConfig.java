6f898eb1a0e7aecd7815fb00997379c64210acea
GSB0203 <gimseungbin0203@gmail.com>
2025-02-22 15:27:21 +0900

    fix(Visit): WebConfig
    방문자 인증에 필요한 쿠키가 발급되지 않는 문제를 해결하기 위해 추가하였습니다.

--- src/main/java/com/airportpus/common/config/WebConfig.java
+++ src/main/java/com/airportpus/common/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://www.airport-pus.kr")
+            .allowedOrigins("*")
             .allowCredentials(true)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
