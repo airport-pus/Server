ffe8fb759fd7861b3706c628cf5a5e2e55ece121
GSB0203 <gimseungbin0203@gmail.com>
2025-03-10 08:34:48 +0900

    fix(Congestion): CongestionSchedulerService
    Transactional과 Scheduled 같이 사용하여 저장이 안 되는 문제를 해결하기 위해 분리하였습니다.

--- src/main/java/com/airportpus/domain/congestion/service/CongestionSchedulerService.java
+++ src/main/java/com/airportpus/domain/congestion/service/CongestionSchedulerService.java
@@ -1,39 +1,22 @@
 package com.airportpus.domain.congestion.service;
 
-import com.airportpus.domain.congestion.domain.Congestion;
-import com.airportpus.domain.congestion.domain.repository.CongestionRepository;
-import com.airportpus.domain.congestion.presentation.dto.RealCongestionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class CongestionSchedulerService {
 
-  private final CongestionService congestionService;
-  private final CongestionRepository congestionRepository;
+  private final CongestionTransactionalService congestionTransactionalService;
 
   @Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
   public void saveCongestion() {
-    RealCongestionResponse response = congestionService.getCongestionRealTime();
-    Congestion congestion = Congestion.builder()
-        .cgdrAllLvl(response.cgdrAllLvl())
-        .cgdrALvl(response.cgdrALvl())
-        .cgdrBLvl(response.cgdrBLvl())
-        .cgdrCLvl(response.cgdrCLvl())
-        .build();
-    congestionRepository.save(congestion);
+    congestionTransactionalService.saveCongestion();
   }
 
   @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
   public void deleteOldCongestionData() {
-    LocalDateTime yesterdayEnd = LocalDate.now().minusDays(1).atTime(23, 59, 59, 999_999_999);
-    congestionRepository.deleteByCreatedAtBefore(yesterdayEnd);
+    congestionTransactionalService.deleteOldCongestion();
   }
 }
