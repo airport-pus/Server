f07947106ad776e58e78565988d627417f1d56bd
GSB0203 <gimseungbin0203@gmail.com>
2025-03-11 10:51:19 +0900

    remove(Apron): SwaggerConfig
    사용하지 않게된 예외를 제거하였습니다.

--- src/main/java/com/airportpus/common/config/SwaggerConfig.java
+++ src/main/java/com/airportpus/common/config/SwaggerConfig.java
@@ -11,26 +11,26 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
     info = @Info(
         title = "Airport-pus API 문서",
-        description =("### 상태 코드 및 오류 메시지 형식:\n"
-            + "| 상태 코드 | 오류 코드 | 설명 |\n"
-            + "| --- | --- | --- |\n"
-            + "| 400 | INVALID_ARGUMENT | 잘못된 값이 들어왔습니다. |\n"
-            + "| 400 | TYPE_MISMATCH | 파라미터 타입이 일치하지 않습니다. |\n"
-            + "| 400 | NULL_VALUE | 필수 값이 누락되었습니다. |\n"
-            + "| 403 | FLIGHT_NUMBER_NOT_FOUND | 항공편명을 찾을 수 없습니다. |\n"
-            + "| 403 | PARKING_LOT_NOT_FOUND | 주차장을 찾을 수 없습니다. |\n"
-            + "| 403 | CONGESTION_NOT_FOUND | 혼잡도를 찾을 수 없습니다. |\n"
-            + "| 500 | XML_PARSING_ERROR | XML 파싱에 실패했습니다. |\n"
-            + "| 500 | SERVER_UNKNOWN | 서버에서 알 수 없는 에러가 발생했습니다. |\n"
-            + "### 오류 메시지 예시:\n"
-            + "```json\n"
-            + "{\n"
-            + "  \"status\": 403,\n"
-            + "  \"errorCode\": \"NULL_VALUE\",\n"
-            + "  \"message\": \"필수 값이 누락되었습니다.\",\n"
-            + "  \"timestamp\": \"2024-09-29T10:00:00\"\n"
-            + "}\n"
-            + "```")
+        description =("""
+            ### 상태 코드 및 오류 메시지 형식:
+            | 상태 코드 | 오류 코드 | 설명 |
+            | --- | --- | --- |
+            | 400 | INVALID_ARGUMENT | 잘못된 값이 들어왔습니다. |
+            | 400 | TYPE_MISMATCH | 파라미터 타입이 일치하지 않습니다. |
+            | 400 | NULL_VALUE | 필수 값이 누락되었습니다. |
+            | 403 | PARKING_LOT_NOT_FOUND | 주차장을 찾을 수 없습니다. |
+            | 403 | CONGESTION_NOT_FOUND | 혼잡도를 찾을 수 없습니다. |
+            | 500 | XML_PARSING_ERROR | XML 파싱에 실패했습니다. |
+            | 500 | SERVER_UNKNOWN | 서버에서 알 수 없는 에러가 발생했습니다. |
+            ### 오류 메시지 예시:
+            ```json
+            {
+              "status": 403,
+              "errorCode": "NULL_VALUE",
+              "message": "필수 값이 누락되었습니다.",
+              "timestamp": "2024-09-29T10:00:00"
+            }
+            ```""")
     )
 )
 public class SwaggerConfig {
