a6521f96bc4719f134b2d456506ffde3028cb5b8
GSB0203 <gimseungbin0203@gmail.com>
2025-03-11 10:44:11 +0900

    fix(Congestion): CongestionSchedulerService
    혼잡도 정보가 저장되지 않는 문제를 확인하기 위해 수정하였습니다.

--- src/main/java/com/airportpus/domain/congestion/service/CongestionSchedulerService.java
+++ src/main/java/com/airportpus/domain/congestion/service/CongestionSchedulerService.java
@@ -1,9 +1,11 @@
 package com.airportpus.domain.congestion.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CongestionSchedulerService {
@@ -12,11 +14,15 @@ public class CongestionSchedulerService {
 
   @Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
   public void saveCongestion() {
+    log.info("start saveCongestion");
     congestionTransactionalService.saveCongestion();
+    log.info("end saveCongestion");
   }
 
-  @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-  public void deleteOldCongestionData() {
+//  @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+  public void deleteOldCongestion() {
+    log.info("start deleteOldCongestion");
     congestionTransactionalService.deleteOldCongestion();
+    log.info("end deleteOldCongestion");
   }
 }
